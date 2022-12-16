8e44bbe377091f8f906ff94d1f9237bab1aff6ff
edalfon <edalfon@users.noreply.github.com>
2022-12-16 06:54:14 +0000

    new url for visa

--- R/visa.R
+++ R/visa.R
@@ -12,11 +12,44 @@
 #'
 #' @return exchange rate (numeric) in terms of 1 currcard = X currtrans
 #' @export
-visa <- function(exchgdate = as.Date(format(Sys.time(),
-                         tz = "US/Pacific")),
-                         currcard = "EUR", 
-                         currtrans = "COP",
-                         trans_amount = 7777777) {
+visa <- function(exchgdate = as.Date(format(Sys.time(), tz = "US/Pacific")),
+                 currcard = "EUR", 
+                 currtrans = "COP",
+                 trans_amount = sample(1e6:9e6, 1)) {
+
+  # Luckily, you can get the data with a properly parametrized get request
+  site_url <- paste0(
+    "https://www.visa.co.uk/cmsapi/fx/rates?",
+    "amount=", trans_amount, "&",
+    "fee=0", "&",
+    "utcConvertedDate=", utils::URLencode(
+        URL = format(exchgdate, format = "%m/%d/%Y"),
+        reserved = TRUE
+    ), "&",
+    "exchangedate=", utils::URLencode(
+        URL = format(exchgdate, format = "%m/%d/%Y"),
+        reserved = TRUE
+    ), "&",
+    "fromCurr=", currcard, "&",
+    "toCurr=", currtrans
+  )
+  
+  # now it's easier, just get a json
+  visa_rates <- jsonlite::fromJSON(site_url)
+  
+  visa_rates$reverseAmount
+}
+
+
+
+
+
+# old versions here, make it faster to check if they go back to any version of
+# the web site, compared to go git my way backwards
+visa_rvest <- function(exchgdate = as.Date(format(Sys.time(), tz = "US/Pacific")),
+                 currcard = "EUR", 
+                 currtrans = "COP",
+                 trans_amount = 7777777) {
 
   # Luckily, you can get the data with a properly parametrized get request
   site_url <- paste0(
@@ -32,7 +65,7 @@ visa <- function(exchgdate = as.Date(format(Sys.time(),
     "toCurr=", currtrans, "&",
     "submitButton=Calculate+exchange+rate"
   )
-  
+    
   # now the page asks to authorize cookies and puts a blank canvas that 
   # does not let rvest to read the contents of the page. So we need to 
   # authorize the cookies. 
@@ -43,7 +76,7 @@ visa <- function(exchgdate = as.Date(format(Sys.time(),
     wscrCookieConsent = "1=true&2=true&3=true&4=true&5=true"
   ))
   rvested <- xml2::read_html(site_url)
-  
+
   rate <- rvested |>
     # CSS selector found using the awesome http://selectorgadget.com/
     # rvest::html_nodes("#er_result_table") |> #whole text, would need wrangling
