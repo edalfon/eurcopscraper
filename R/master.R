261ce11e6f046a2e16f58e01141816bebc720656
edalfon <edalfon@users.noreply.github.com>
2023-07-09 20:33:54 +0000

    master, use again a get request

--- R/master.R
+++ R/master.R
@@ -14,7 +14,7 @@
 #' @export
 #' @examples later
 #' later
-master <- function(exchgdate = as.Date(format(Sys.time(), 
+master_puppeteer <- function(exchgdate = as.Date(format(Sys.time(), 
                    tz = "US/Pacific")),
                    currcard = "EUR", 
                    currtrans = "COP") {
@@ -37,4 +37,46 @@ master <- function(exchgdate = as.Date(format(Sys.time(),
   master_rate <- 7777777 / master_conv # this is hard coded in master.js
 
   master_rate
+}
+
+#' Scraps currency exchange information for transactions using credit cards
+#' issued by a MasterCard Europe bank.
+#'
+#' This used to work by a simple GET request with a properly parametrized URL
+#' However, it stoped to work some time ago. So I had to move to a puppeteer 
+#' based scraping approach.
+#'
+#' @param exchgdate transaction date, format YYYY-mm-dd
+#' @param crdhldBillCurr currency of the card
+#' @param transCurr currency of the transaction
+#' @param bankFee bank fee, [0, 1]
+#' @trans_amount transaction amount (integer)
+#'
+#' @return exchange rate (numeric) in terms of transCurr / crdhldBillCurr
+#' @export
+#' @examples later
+#' later
+master <- function(
+    exchgdate = as.Date(format(Sys.time(), tz = "US/Pacific")),
+    crdhldBillCurr = "EUR", 
+    transCurr = "COP",
+    bankFee = 0,
+    trans_amount = sample(1e6:9e6, 1)
+  ) {
+                  
+  # Luckily, you can AGAIN get the data with a properly parametrized get request
+  site_url <- paste0(
+    "https://www.mastercard.co.uk/settlement/currencyrate/conversion-rate?",
+    "fxDate=", exchgdate, "&",
+    "transCurr=", transCurr, "&",
+    "crdhldBillCurr=", crdhldBillCurr, "&",
+    "bankFee=", bankFee, "&",
+    "transAmt=", trans_amount
+  )
+
+  # now it's easier, just get a json
+  master_rates <- jsonlite::fromJSON(site_url)
+
+  # master_rates$data$conversionRate
+  trans_amount / master_rates$data$crdhldBillAmt
 }
