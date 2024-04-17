f659e5d7d334b888f02014255724048b62366cfb
edalfon <edalfon@users.noreply.github.com>
2024-04-17 08:48:28 +0000

    fix: just skip visa when it fails

--- eurcopscraper.R
+++ eurcopscraper.R
@@ -1,4 +1,4 @@
-# just to quickly test 
+# just to quickly test
 
 lapply(list.files("./R", full.names = TRUE), source, encoding = "UTF-8")
 
@@ -8,9 +8,17 @@ vancouver_df <- vancouver()
 vancouver_df$timestamp <- timestamp
 appendRDS("data/vancouver.rds", vancouver_df)
 
-visa_rate <- visa()
-visa_df <- data.frame(visa_rate = visa_rate, timestamp = timestamp)
-appendRDS("data/visa.rds", visa_df)
+# there are issues with visa, basically a 403 forbidden error
+# there must be some sort of rate-limit going on, that applies to the IP address
+# from github servers or something (already tried user agent and other headers)
+tryCatch(
+  {
+    visa_rate <- visa()
+    visa_df <- data.frame(visa_rate = visa_rate, timestamp = timestamp)
+    appendRDS("data/visa.rds", visa_df)
+  },
+  error = \(e) cat("Visa Error:", conditionMessage(e), "\n")
+)
 
 master_rate <- master()
 master_df <- data.frame(master_rate = master_rate, timestamp = timestamp)
@@ -34,4 +42,3 @@ appendRDS("data/kapital.rds", kapital_df)
 
 
 quarto::quarto_render("quarto", as_job = FALSE, execute_dir = ".")
-
