9bafdb85418fde39c989a96da9f5b4f9bdb813cd
edalfon <edalfon@users.noreply.github.com>
2024-08-02 10:34:27 +0000

    fix: kapital updating html_nodes selectors

--- R/kapital.R
+++ R/kapital.R
@@ -1,4 +1,3 @@
-
 #' Scraps currency exchange information from https://cambioskapital.com/
 #'
 #' Check out this link:
@@ -7,33 +6,35 @@
 #' @return exchange rate (numeric) in terms of COP/EUR
 #' @export
 kapital <- function() {
-  
   # scrap the information
   rvested <- xml2::read_html("https://cambioskapital.com/")
   # this is just for the screenshot. Should we use this html to avoid two requests?
   kapital_html <- system2(
-    command = "node", 
-    args = c("JS/screenshot.js", "https://cambioskapital.com/", "logs/kapital.png"), 
+    command = "node",
+    args = c("JS/screenshot.js", "https://cambioskapital.com/", "logs/kapital.png"),
     stdout = TRUE
   )
 
-  compra <- rvested |> 
-    rvest::html_nodes(".banderpre1:nth-child(2) b") |>
+  compra <- rvested |>
+    rvest::html_nodes(".elementor-element-1889b04 .jet-listing-dynamic-field__content") |>
+    # rvest::html_nodes(".banderpre1:nth-child(2) b") |>
     rvest::html_text() |>
     stringr::str_remove("\\.") |>
-    stringr::str_remove("\\$") |> 
+    stringr::str_remove("\\$") |>
+    stringr::str_remove("Compramos: ") |>
     as.numeric()
 
-  venta <- rvested |> 
-    rvest::html_nodes("br~ .banderpre1 b") |>
+  venta <- rvested |>
+    rvest::html_nodes(".elementor-element-a070864 .jet-listing-dynamic-field__content") |>
     rvest::html_text() |>
     stringr::str_remove("\\.") |>
-    stringr::str_remove("\\$") |> 
+    stringr::str_remove("\\$") |>
+    stringr::str_remove("Vendemos: ") |>
     as.numeric()
 
-  moneda <- rvested |> 
-    rvest::html_nodes("#eluidd9ec0bb1 strong") |>
-    rvest::html_text()  
+  moneda <- rvested |>
+    rvest::html_nodes(".elementor-element-9cfe99b .elementor-size-default") |>
+    rvest::html_text()
 
   ratesdf <- data.frame(moneda = moneda, compra = compra, venta = venta)
 
