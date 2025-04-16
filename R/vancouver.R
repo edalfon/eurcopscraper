6ab1e10c7f51a117bc625835e076f43969a525ba
edalfon <edalfon@gmail.com>
2025-04-16 12:00:20 +0200

    fix: in vancouver go back to scrape the table

--- R/vancouver.R
+++ R/vancouver.R
@@ -12,42 +12,42 @@ vancouver <- function() {
   # selectorgadget.com, but it ends up being less mantainable
   # (e.g. new currency changes the order of the table), so let's scrape
   # now the whole table
-  # exchangetable <- rvest::html_table(rvested, convert = FALSE) |>
-  #   dplyr::bind_rows() |> # for some reason, it gets two tables
-  #   dplyr::distinct() |> # so let's just append them and remove duplicates
-  #   dplyr::mutate(dplyr::across(
-  #     .cols = dplyr::everything(),
-  #     .fns = ~ .x |>
-  #       gsub(.x, pattern = ",", replacement = "") |>
-  #       gsub(pattern = "\\.", replacement = "") |>
-  #       gsub(pattern = "\\$", replacement = "") |>
-  #       stringr::str_trim()
-  #   )) |>
-  #   dplyr::mutate(dplyr::across(dplyr::contains("Valor"), .fns = as.numeric)) |>
-  #   janitor::clean_names()
+  exchangetable <- rvest::html_table(rvested, convert = FALSE) |>
+    dplyr::bind_rows() |> # for some reason, it gets two tables
+    dplyr::distinct() |> # so let's just append them and remove duplicates
+    dplyr::mutate(dplyr::across(
+      .cols = dplyr::everything(),
+      .fns = ~ .x |>
+        gsub(.x, pattern = ",", replacement = "") |>
+        gsub(pattern = "\\.", replacement = "") |>
+        gsub(pattern = "\\$", replacement = "") |>
+        stringr::str_trim()
+    )) |>
+    dplyr::mutate(dplyr::across(dplyr::contains("Valor"), .fns = as.numeric)) |>
+    janitor::clean_names()
 
   # gotta go back to using selectors, because the whole table, there is always
   # like a placeholder table there that never changes values
-  exchangetable <- rvested |>
-    rvest::html_nodes(".e-con-inner .e-grid") |>
-    rvest::html_text2() |>
-    unique() |>
-    stringr::str_replace("País\nMoneda", "País-Moneda") |>
-    stringr::str_replace_all(pattern = "\n", replacement = "\t") |>
-    paste0(collapse = "\n") |>
-    readr::read_tsv(
-      col_names = c("moneda", "valor_compra", "valor_venta"),
-      skip = 1
-    ) |>
-    dplyr::mutate(
-      moneda = dplyr::case_when(
-        moneda == "Euro 100 (EUR)" ~ "Euro  (EUR) 100",
-        moneda == "Euro 200 (US)" ~ "Euro (EUR) 200",
-        moneda == "Euro 50 (EUR)" ~ "Euro (EUR) 50",
-        moneda == "Euro 500 (US)" ~ "Euro (EUR) 500",
-        TRUE ~ moneda
-      )
-    )
+  # exchangetable <- rvested |>
+  #   rvest::html_nodes(".e-con-inner .e-grid") |>
+  #   rvest::html_text2() |>
+  #   unique() |>
+  #   stringr::str_replace("País\nMoneda", "País-Moneda") |>
+  #   stringr::str_replace_all(pattern = "\n", replacement = "\t") |>
+  #   paste0(collapse = "\n") |>
+  #   readr::read_tsv(
+  #     col_names = c("moneda", "valor_compra", "valor_venta"),
+  #     skip = 1
+  #   ) |>
+  #   dplyr::mutate(
+  #     moneda = dplyr::case_when(
+  #       moneda == "Euro 100 (EUR)" ~ "Euro  (EUR) 100",
+  #       moneda == "Euro 200 (US)" ~ "Euro (EUR) 200",
+  #       moneda == "Euro 50 (EUR)" ~ "Euro (EUR) 50",
+  #       moneda == "Euro 500 (US)" ~ "Euro (EUR) 500",
+  #       TRUE ~ moneda
+  #     )
+  #   )
 
   # Ths would be based on pupetteer
   # vancouver_html <- system2(
